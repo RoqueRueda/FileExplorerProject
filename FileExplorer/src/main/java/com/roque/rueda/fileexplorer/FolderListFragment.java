2d785222a3f7f5c46fd24f88222068a97c94ad89
RoqueRueda <skaroque@gmail.com>
2013-10-27 14:37:18 -0600

    Start of the project
    This project will be a simple file explorer dialog for the notepad
    application

---
+++ FileExplorer/src/main/java/com/roque/rueda/fileexplorer/FolderListFragment.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2013 Roque Rueda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.roque.rueda.fileexplorer;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Roque on 27/10/13.
+ *
+ * This fragment its used to display the list of folders.
+ *
+ *
+ */
+public class FolderListFragment extends Fragment {
+
+    public FolderListFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+
+}
